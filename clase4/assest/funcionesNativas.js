4ed5197d584cbee4950d612da99d0143afc9416d
Daniel Valencia <charaprogram19@gmail.com>
2022-07-22 09:56:48 -0500

    cambie nombre de fundamentos por calse 1. y aprendimos funciones nativas para numeros

---
+++ clase4/assest/funcionesNativas.js
@@ -0,0 +1,16 @@
+let saludo ='hola charanga'
+ console.log(saludo);
+ console.log(saludo.length) // devuelve la cantidad de caracteres
+
+ console.log(saludo.toUpperCase()) // devuelve el texto en mayusculas
+ console.log(saludo.toLocaleLowerCase()) // devuelve el texto en minusculas
+
+ console.log(saludo.slice(0,4)) // devuelve fragmento comprendido entre las posicionesproporcionadas por parametros
+ console.log(saludo.slice(5,13))
+
+console.log(saludo.includes('mundo')) //devuelve verdadero o falaso si la cedena existe
+console.log(saludo.indexOf('a'))//devuelve la primera posicion en la que aparezca la cadena
+console.log(saludo.replace('hola charanga','hola soy una maquina')) //perimite reemplazar
+let numero = 5
+console.log(typeof saludo.toString())
+console.log(typeof numero)
---
+++ clase4/assest/funcionesNumeros.js
@@ -0,0 +1,22 @@
+console.log(Math.E)// devuelve la constante de euler
+
+console.log(Math.PI)// devuelve el resultado de pi
+
+console.log(Math.pow(2,2))
+console.log(2**3)
+
+let numer1 = 25 ;
+let numer2 = 90;
+
+console.log(Math.min(numer1,numer2))
+console.log(Math.max(numer1,numer2))
+ const numerpi=3.1416
+ console.log(Math.round(numerpi))
+ console.log(Math.ceil(numerpi))
+ console.log(Math.trunc(Math.random()*(numer2-numer1)+numer1))
+
+ const numeros = [0, 20,  10, 5, 30]
+
+ console.log(typeof numeros)
+ console.log(`el numero menor es: ${Math.min(...numeros)}`);
+ console.log(`el numero mayor es : ${Math.max(...numeros)}`);
