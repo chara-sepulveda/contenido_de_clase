465d64bce32c9497eac526a9b05e5bbd30d36779
Daniel Valencia <charaprogram19@gmail.com>
2022-07-25 09:05:22 -0500

    lo que llevamos de la clase 5

---
+++ clase5/assest/app.js
@@ -0,0 +1,8 @@
+let ishungry = true;
+let isSunny = false;
+console.log(ishungry);
+console.log(isSunny);
+    
+    let num1=5,num2='5';
+console.log(num1==num2) // valida si son iguales
+console.log(num1===num2) // valida si son iguales y 
