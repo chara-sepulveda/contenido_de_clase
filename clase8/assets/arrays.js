bd0ddb2d855ec5a6af5e5bdc4c8b8d377cb17ec6
Daniel Valencia <charaprogram19@gmail.com>
2022-08-02 09:21:09 -0500

    hicimos repaso de condicional y funciones

---
+++ clase8/assets/arrays.js
@@ -0,0 +1,10 @@
+let numeros=[2,3,9,5,6,1]
+console.log(numeros)
+console.log(numeros.sort())
+numeros.push(10)
+console.log(numeros)
+numeros.pop()
+console.log(numeros)
+numeros.shift(10)
+console.log(numeros.length
+    )
---
+++ clase9/assest/alcance.js
@@ -0,0 +1,20 @@
+let num=1;
+console.log(num)
+
+const saludar=() =>{
+    let num2=5;
+    console.log(num*num2)
+}
+saludar()
+
+const validadEdad =()=>{
+    let edad=19;
+    if( edad>18){
+        let mensaje="wakanda for ever"
+        console.log("es mayor de edad")
+        console.log(mensaje)
+    }else{
+        console.log("estas sardino")
+    }
+}
+validadEdad()
